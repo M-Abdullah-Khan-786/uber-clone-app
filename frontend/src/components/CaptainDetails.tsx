6d6815f21b6352c891dd0e44e3ab1849ebdd8a73
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-01-02 19:16:55 +0500

    Driver Home Page UI

---
+++ frontend/src/components/CaptainDetails.tsx
@@ -0,0 +1,45 @@
+import { FaBook } from "react-icons/fa6";
+import { IoIosSpeedometer, IoIosTime } from "react-icons/io";
+
+const CaptainDetails = () => {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center justify-start gap-3">
+          <img
+            className="h-10 w-10 rounded-full object-cover"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5eW8OvSN4zaimWuFO2ff4Q-Es1FS8ajh4WA&s"
+            alt=""
+          />
+          <h4 className="text-lg font-medium">Ahmad</h4>
+        </div>
+        <div>
+          <h4 className="text-xl font-semibold">Rs200</h4>
+          <p className="text-sm  text-gray-600">Earned</p>
+        </div>
+      </div>
+      <div className="flex justify-center gap-8 items-center p-3 mt-8 bg-gray-100 rounded-xl">
+        <div className="text-center">
+          <IoIosTime className="text-3xl mb-2 font-thin inline-block" />
+          <h5 className="text-lg font-medium">10.20</h5>
+          <p className="text-sm text-gray-600">Hours Online</p>
+        </div>
+
+        <div className="text-center">
+          <IoIosSpeedometer className="text-3xl mb-2 font-thin inline-block" />
+
+          <h5 className="text-lg font-medium">10.20</h5>
+          <p className="text-sm text-gray-600">Hours Online</p>
+        </div>
+        <div className="text-center">
+          <FaBook className="text-3xl mb-2 font-thin inline-block" />
+          <h5 className="text-lg font-medium">10.20</h5>
+
+          <p className="text-sm text-gray-600">Hours Online</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CaptainDetails;
--- frontend/src/pages/CaptainHome.tsx
+++ frontend/src/pages/CaptainHome.tsx
@@ -1,8 +1,37 @@
+import { RiLogoutCircleFill } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import CaptainDetails from "../components/CaptainDetails";
 
 const CaptainHome = () => {
   return (
-    <div>CaptainHome</div>
-  )
-}
+    <>
+      <div className="h-screen">
+        <div className="fixed p-6 top-0 flex items-center justify-between w-screen">
+          <img
+            className="w-16"
+            src="https://download.logo.wine/logo/Uber/Uber-Logo.wine.png"
+            alt=""
+          />
+          <Link
+            to="/home"
+            className="h-10 w-10 bg-white flex items-center justify-center rounded-full"
+          >
+            <RiLogoutCircleFill className="text-2xl font-medium" />
+          </Link>
+        </div>
+        <div className="h-3/5">
+          <img
+            className="h-full w-full"
+            src="https://i0.wp.com/www.medianama.com/wp-content/uploads/2018/06/Screenshot_20180619-112715.png.png?fit=493%2C383&ssl=1"
+            alt=""
+          />
+        </div>
+        <div className="h-2/5 p-6">
+          <CaptainDetails />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default CaptainHome
+export default CaptainHome;
