94113465a2218054e66d05edd6d7133a36d0a525
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-01-03 19:11:31 +0500

    Ride PopUp Pannel UI

---
+++ frontend/src/components/RidePopupPannel.tsx
@@ -0,0 +1,76 @@
+import { BsCash } from "react-icons/bs";
+import { FaLocationDot } from "react-icons/fa6";
+import { GrLocationPin } from "react-icons/gr";
+import { IoIosArrowDown } from "react-icons/io";
+
+interface RidePopupPannelProps {
+  setConfirmRidePanel: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const RidePopupPannel: React.FC<RidePopupPannelProps> = ({
+  setConfirmRidePanel,
+}) => {
+  return (
+    <>
+      <h5
+        onClick={() => {
+          setConfirmRidePanel(false);
+        }}
+        className="p-1 text-center w-[93%] absolute top-0  flex justify-center items-center"
+      >
+        <IoIosArrowDown className="text-3xl text-gray-400" />
+      </h5>
+
+      <h3 className="text-2xl font-semibold mb-5">New Ride!</h3>
+      <div className="flex items-center  justify-between bg-gray-200 rounded-lg p-3 mt-4">
+        <div className="flex items-center gap-3">
+          <img
+            className="h-10 w-10 rounded-full object-cover"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5eW8OvSN4zaimWuFO2ff4Q-Es1FS8ajh4WA&s"
+            alt=""
+          />
+          <h2 className="text-lg font-medium">Usman</h2>
+        </div>
+        <h5 className="text-lg font-semibold">2.2 KM</h5>
+      </div>
+      <div className="flex flex-col gap-2 justify-between items-center">
+        <div className="w-full mt-5">
+          <div className="flex items-center gap-5 p-3 border-b-2">
+            <FaLocationDot className="text-lg" />
+            <div>
+              <h3 className="text-lg font-medium">Address</h3>
+              <p className="text-sm text-gray-600">Example Address</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-5 p-3 border-b-2">
+            <GrLocationPin className="text-lg" />
+            <div>
+              <h3 className="text-lg font-medium">Address</h3>
+              <p className="text-sm text-gray-600">Example Address</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-5 p-3">
+            <BsCash className="text-lg" />
+            <div>
+              <h3 className="text-lg font-medium">Rs 220</h3>
+              <p className="text-sm text-gray-600">Cash</p>
+            </div>
+          </div>
+        </div>
+        <button className="w-full bg-black text-white font-semibold rounded-lg p-2 mt-5">
+          Confirm
+        </button>
+        <button
+          onClick={() => {
+            setConfirmRidePanel(false);
+          }}
+          className="w-full bg-red-400 hover:bg-red-600 text-white font-semibold rounded-lg p-2 mt-5"
+        >
+          Ignore
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default RidePopupPannel;
--- frontend/src/pages/DriverHome.tsx
+++ frontend/src/pages/DriverHome.tsx
@@ -1,8 +1,31 @@
 import { RiLogoutCircleFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import DriverDetails from "../components/DriverDetails";
+import RidePopupPannel from "../components/RidePopupPannel";
+import { useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const DriverHome = () => {
+  const [confirmRidePanel, setConfirmRidePanel] = useState(false);
+
+  const confirmRidePanelRef = useRef(null);
+
+  useGSAP(
+    function () {
+      if (confirmRidePanel) {
+        gsap.to(confirmRidePanelRef.current, {
+          transform: "translateY(0)",
+        });
+      } else {
+        gsap.to(confirmRidePanelRef.current, {
+          transform: "translateY(100%)",
+        });
+      }
+    },
+    [confirmRidePanel]
+  );
+
   return (
     <>
       <div className="h-screen">
@@ -11,6 +34,9 @@ const DriverHome = () => {
             className="w-16"
             src="https://download.logo.wine/logo/Uber/Uber-Logo.wine.png"
             alt=""
+            onClick={()=>{
+              setConfirmRidePanel(true)
+            }}
           />
           <Link
             to="/home"
@@ -28,6 +54,12 @@ const DriverHome = () => {
         </div>
         <div className="h-2/5 p-6">
           <DriverDetails />
+        </div>
+        <div
+          ref={confirmRidePanelRef}
+          className="w-full fixed z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-14 "
+        >
+          <RidePopupPannel setConfirmRidePanel={setConfirmRidePanel} />
         </div>
       </div>
     </>
